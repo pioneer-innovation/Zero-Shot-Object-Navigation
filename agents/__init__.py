a9b10058faa9579f77b17db92cb655f526b0bdcc
pioneer-innovation <54060693+pioneer-innovation@users.noreply.github.com>
2022-06-15 16:08:40 +0800

    Add files via upload

---
+++ agents/__init__.py
@@ -0,0 +1,11 @@
+from .navigation_agent import NavigationAgent
+from .random_agent import RandomNavigationAgent
+from .semantic_agent import SemanticAgent
+
+__all__ = [
+    'NavigationAgent',
+    'RandomNavigationAgent',
+    'SemanticAgent',
+]
+
+variables = locals()
